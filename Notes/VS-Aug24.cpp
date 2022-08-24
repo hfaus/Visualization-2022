3f7cd7e5988a7267cff97434513cdf6eb8fa1583
hfaus <70163503+hfaus@users.noreply.github.com>
2022-08-24 15:40:20 -0400

    Class notes from Aug 24

---
+++ Notes/VS-Aug24.cpp
@@ -0,0 +1,44 @@
+/**
+ * Three types of memory
+ *  - Sensory : Pre-attentive processing
+ *  - Short team : 
+ *  - 
+ * 
+ * Gestalt Laws/Principles
+ * - Gestalt : German word roughtly translated as form, pattern or configuration
+ * - Humans perceive relationships/patterns
+ * - Principles :
+ *      - Proximity : immediately group objects closer together, assume relation
+ *           * * * * * * *  vs ** ** ** ** **
+ *              ex - scatterplot
+ *      - Similarity : similar shape, size, color or texture, assume relation
+ *              * * * $ $ $ & $ & & & &
+ *      - Closure : we fill in the blanks, infer outline of shape
+ *              ex - the WWF panda
+ *      - Enclosure : enclose data in some way to show user its importance
+ *              ex - circlue or shade a portion of data
+ *      - Continuity : objects or shpae that are arranged in a line or a curve are perceived to be more related than those that are abrupt
+ *              *
+ *               *
+ *                *
+ *              * * * * * * * *
+ *                  *
+ *                   *
+ *                    *
+ *      - Figure-Ground : based on the color, contrast, size objects are separated as whole figures from their backgrounds
+ *              ex - those photos where you can see a young or old woman
+ * 
+ * Short term memory
+ * - Stays around for a min
+ * - Can increase by use of chunking (grouping similar things together)
+ * 
+ * Change blindness
+ * - Failure to notice a difference in successive images
+ * - Take away : need to be very clear when presenting differences 
+ *      ex - scientific graphs
+ * 
+ * Long term memory
+ * - Not directly involved in visualization process, but still has an effect
+ * 
+ * Do not use red and green together, color blind people can't see
+ */
